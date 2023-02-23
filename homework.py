1a5222be0c1bd921fb41fb35f0ad900c3f69ed0c
Алиса Белич <alisacalm@yandex.ru>
2023-02-23 23:28:36 +0500

    Ревью № 3. Тесты

--- homework.py
+++ homework.py
@@ -15,7 +15,7 @@ import exceptions
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN'),
-TELEGRAM_TOKEN: str = os.getenv('TELEGRAM_TOKEN'),
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN'),
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 env_var = {
